d5287e4a7d0c9a6b0cdc765ac56e0df7a468b1de
Cassidy Lim <88170408+cassuhdeel@users.noreply.github.com>
2022-02-08 00:56:30 -0500

    Deleted seeds
    Deleted files not needed

--- side-project/server/db/seeds/00-owners.js
+++
@@ -1,9 +0,0 @@
-const owners = require("../fixtures/owners");
-
-exports.seed = function (knex) {
-  return knex
-    .raw("TRUNCATE TABLE owners RESTART IDENTITY CASCADE")
-    .then(function () {
-      return knex("owners").insert(owners);
-    });
-};
--- side-project/server/db/seeds/01-pets.js
+++
@@ -1,9 +0,0 @@
-const pets = require("../fixtures/pets");
-
-exports.seed = function (knex) {
-  return knex
-    .raw("TRUNCATE TABLE pets RESTART IDENTITY CASCADE")
-    .then(function () {
-      return knex("pets").insert(pets);
-    });
-};
