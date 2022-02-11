1058f544c4926220b352a2a4151b3cb8dcd5ae5c
Jason Lee <idforvy@gmail.com>
2022-02-11 18:14:43 -0500

    form implemented

--- frontend/src/clicker/Clicker2.js
+++ frontend/src/clicker/Clicker2.js
@@ -1,20 +1,56 @@
 import React, { useState } from "react";
 
 function Clicker2() {
+  const initialForm = {
+    name: "Name",
+    counter: 0
+  };
+  const [formData, setFormData] = useState(initialForm);
 
-  const [counter, setCounter] = useState(0) 
+  const clickerHandler = () => {
+    setFormData(({counter}) => ({
+      ...formData,
+      counter: counter + 2
+    }))
+  };
 
-const clickerHandler = () => {
-  setCounter(counter + 2)
-}
+  const changeHandler = (event) => {
+    setFormData= ({
+      [event.target.name]: event.target.value
+    })
+  }
 
   return (
     <div style={{ color: "green", fontSize: 30 }}>
       This is the SECOND 'Clicker' Page
       <div>
-        <button className="btn btn-secondary" onClick={clickerHandler} >
-          Clicker 2: {counter}
+        <button className="btn btn-secondary" onClick={clickerHandler}>
+          Clicker 2: {formData.counter}
         </button>
+      </div>
+      <div>
+        <form>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              name="name"
+              id="name"
+              type="text"
+              value={formData.name}
+              onChange={changeHandler}
+            />
+          </div>
+          <div>
+            <label htmlFor="counter">Counter</label>
+            <input
+              name="counter"
+              id="counter"
+              type="number"
+              value={formData.counter}
+              onChange={changeHandler}
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
