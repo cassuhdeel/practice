ff35e62c2fc73d0a982053c4f26d58ce4dd2596b
Cassidy Lim <cassidylim7@gmail.com>
2022-02-11 17:33:03 -0500

    Added clicker2 logic to frontend

--- frontend/src/clicker/Clicker2.js
+++ frontend/src/clicker/Clicker2.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Clicker2() {
+
+  const [counter, setCounter] = useState(0) 
+
+const clickerHandler = () => {
+  setCounter(counter + 2)
+}
+
   return (
     <div style={{ color: "green", fontSize: 30 }}>
       This is the SECOND 'Clicker' Page
+      <div>
+        <button className="btn btn-secondary" onClick={clickerHandler} >
+          Clicker 2: {counter}
+        </button>
+      </div>
     </div>
   );
 }
