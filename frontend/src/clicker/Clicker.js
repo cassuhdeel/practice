e69053cca912234d1233faa9f918786af0e5cbf5
Jason Lee <idforvy@gmail.com>
2022-02-11 17:15:47 -0500

    edited Clicker2 and added clicker function

--- frontend/src/clicker/Clicker.js
+++ frontend/src/clicker/Clicker.js
@@ -1,11 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Clicker() {
+  const [counter, setCounter] = useState(0)
+
+  const navigate = useNavigate();
+
+  const buttonHandler = () => {
+    setCounter(counter + 1)
+  };
+
+  const goClicker2 = () => {
+    navigate('/clicker/clicker2')
+  }
+  
   return (
     <div>
-      This is the 'Clicker' Page
-      <Link to="/clicker/clicker2">Go to Clicker2</Link>
+      <p className="mb-4">This is the 'Clicker' Page</p>
+      <h4>Clicks: {counter}</h4>
+      <button className="btn btn-primary" onClick={buttonHandler}>
+        Add Click
+      </button>
+      <div>
+        <button onClick={goClicker2}>Go to Clicker2</button>
+      </div>
     </div>
   );
 }
