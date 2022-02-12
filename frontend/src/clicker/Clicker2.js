cddc8fb04ccc271915c76733aa1aedcc9b59b6cc
Jason Lee <idforvy@gmail.com>
2022-02-12 09:00:56 -0500

    edited change handler

--- frontend/src/clicker/Clicker2.js
+++ frontend/src/clicker/Clicker2.js
@@ -15,8 +15,10 @@ function Clicker2() {
   };
 
   const changeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value
     setFormData= ({
-      [event.target.name]: event.target.value
+      [name]: value
     })
   }
 
